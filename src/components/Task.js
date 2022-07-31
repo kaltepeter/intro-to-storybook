75a20c8e3aa67e98c07f00402fbcc1dc81722294
Kayla Altepeter <kayla.altepeter@datasite.com>
2022-07-31 10:35:41 -0500

    Creating task list, composite component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export default function Task({
   task: { id, title, state },
@@ -23,12 +23,24 @@ export default function Task({
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <label htmlFor="title" aria-label={title} className="title">
-        <input type="text" value={title} readOnly={true} name="title" placeholder="Input title" />
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Input title"
+        />
       </label>
 
-      {state !== 'TASK_ARCHIVED' && (
-        <button className="pin-button" onClick={() => onPinTask(id)} id={`pinTask-${id}`} aria-label={`pinTask-${id}`} key={`pinTask-${id}`}>
-            <span className={`icon-star`} />
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
         </button>
       )}
     </div>
@@ -36,17 +48,17 @@ export default function Task({
 }
 
 Task.propTypes = {
-     /** Composition of the task */
-     task: PropTypes.shape({
-       /** Id of the task */
-       id: PropTypes.string.isRequired,
-       /** Title of the task */
-       title: PropTypes.string.isRequired,
-       /** Current state of the task */
-       state: PropTypes.string.isRequired,
-     }),
-     /** Event to change the task to archived */
-     onArchiveTask: PropTypes.func,
-     /** Event to change the task to pinned */
-     onPinTask: PropTypes.func,
-    };
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
